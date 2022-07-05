203dff0599c342a963d86506a986d38aaecfd58e
Mustan Ali <mustan9900@gmail.com>
2022-07-05 11:35:07 +0500

    added Binary Search Tree code

---
+++ DSA/src/BinarySearchTree/Main.java
@@ -0,0 +1,113 @@
+/*
+Insertion
+Deletion
+*/
+package BinarySearchTree;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BinarySearchTree tree = new BinarySearchTree();
+
+        tree.insert(8);
+        tree.insert(3);
+        tree.insert(1);
+        tree.insert(6);
+        tree.insert(7);
+        tree.insert(10);
+        tree.insert(14);
+        tree.insert(4);
+
+        System.out.print("Inorder traversal: ");
+        tree.inorder();
+
+        System.out.println("\nAfter deleting 10");
+        tree.deleteKey(10);
+
+        System.out.print("Inorder traversal: ");
+        tree.inorder();
+    }
+}
+
+class BinarySearchTree {
+    class Node {
+        int key;
+        Node left, right;
+
+        public Node(int item) {
+            key = item;
+            left = right = null;
+        }
+    }
+
+    Node root;
+
+    void insert(int key) {
+        root = insertKey(root, key);
+    }
+
+
+    Node insertKey(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+        if (key < root.key)
+            root.left = insertKey(root.left, key);
+        else if (key > root.key)
+            root.right = insertKey(root.right, key);
+
+        return root;
+    }
+
+
+    // Inorder Traversal
+    void inorderRec(Node root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.print(root.key + " ");
+            inorderRec(root.right);
+        }
+    }
+
+
+    void inorder() {
+        inorderRec(root);
+    }
+
+
+    int minValue(Node root) {
+        int minValue = root.key;
+        while (root.left != null) {
+            minValue = root.left.key;
+            root = root.left;
+        }
+        return minValue;
+    }
+
+
+    Node deleteRec(Node root, int key) {
+        if (root == null)
+            return root;
+
+        if (key < root.key)
+            root.left = deleteRec(root.left, key);
+        else if (key > root.key)
+            root.right = deleteRec(root.right, key);
+        else {
+            if (root.left == null)
+                return root.right;
+            else if (root.right == null)
+                return root.left;
+
+            root.key = minValue(root.right);
+            root.right = deleteRec(root.right, root.key);
+        }
+        return root;
+    }
+
+
+    void deleteKey(int key) {
+        root = deleteRec(root, key);
+    }
+}
